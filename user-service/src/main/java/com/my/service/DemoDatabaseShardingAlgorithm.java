4144d54e766acd126234627c4c2b5750f7baf198
wangqian <wangqian@xwbank.com>
2018-09-06 17:56:48 +0800

    version01, no dubbo,no zookeeper

---
+++ user-service/src/main/java/com/my/service/DemoDatabaseShardingAlgorithm.java
@@ -0,0 +1,19 @@
+package com.my.service;
+
+import com.google.common.collect.Range;
+import io.shardingsphere.core.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingsphere.core.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+import java.util.Collection;
+public class DemoDatabaseShardingAlgorithm implements PreciseShardingAlgorithm<Long> {
+    @Override
+    public String doSharding(Collection<String> collection, PreciseShardingValue<Long> preciseShardingValue) {
+        for (String each : collection) {
+            if (each.endsWith(Long.parseLong(preciseShardingValue.getValue().toString()) % 2+"")) {
+                return each;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+}
+
